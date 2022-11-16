888c9a41d9c4482431da0351924c91c86b065c68
mrinal kaushik <mrinalrocks24x7gmail.com>
2022-11-16 17:53:56 +0530

    ADD:sum of n natural numbers eg. Input N=5 output 1+2+3+4+5

---
+++ Sumnum.java
@@ -0,0 +1,16 @@
+package day2project;
+
+public class Sumnum {
+	public static void main(String[] args) {
+		int n = 1, sum = 0;
+
+		while (n <= 10) {
+			sum = sum + n;
+			n++;
+		}
+		System.out.println("sum=" + sum);
+	}
+
+}
+
+
